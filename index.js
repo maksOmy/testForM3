4b36841073b51b3869d36697566f95339bde627c
maksOmy <antipov.maks00@mail.ru>
2020-11-08 13:41:45 +0400

    create cards

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 
-$.ajax({ url: "https://api.github.com/users" }).done((userList) => {
-    $.each(userList, (index, value) => {
-        const container = $('<main>');
-        container.append('<div>', {
-            text: 'hello'
-        })
-        console.log(container)
+const addCards = () => {
+    $.ajax({ url: "https://api.github.com/users" }).done((userList) => {
+    $.each(userList, (index, user) => {
+        const container = $('.container');     
+        
+        $.ajax({url: user.url}).done((userInfo) => {
+            const cardItem = '<div class="card-item">' +
+                          `<a href="${user.html_url}" class="avatar"><img src="${user.avatar_url}"></a>` +
+                          `<p class="username">Username: ${user.login}</p>` +
+                          `<span>Public Repositories Count: ${userInfo.public_repos}</span>` +
+                         '</div>';
+            container.append(cardItem);
+
+            $('.card-item').on('click', () => {
+                console.log(this)
+                $(this).addClass('fullscreen-card-item');
+            });
+        });
+        
     });
 });
+};
+
+addCards();
+
+$('<body>').on('scroll', () => {
+    console.log('cool')
+    addCards();
+})
+
+
+
+
+console.log()
