25c4abb91e84e21a7af498b94a1caeb50eeea32e
maksOmy <antipov.maks00@mail.ru>
2020-11-10 15:19:42 +0400

    refactoring

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-
 const addCards = (usersUrl) => {
     $.ajax({ url: usersUrl }).done((userList) => {
         $.each(userList, (index, user) => {
             $.ajax({ url: user.url }).done((userInfo) => {
                 const cardItem = `<div id="${userInfo.id}" class="card-item">` +
-                    `<a href="${user.html_url}" class="avatar"><img src="${user.avatar_url}"></a>` +
+                    `<a href="${user.html_url}" class="avatar"><img src="${user.avatar_url}" alt="user avatar"></a>` +
                     `<p class="username">Username: ${user.login}</p>` +
                     `<p>Public Repositories Count: ${userInfo.public_repos}</p>` +
                     '<div class="close-btn"></div>' +
                     '</div>';
-                    $('.container').append(cardItem);                
+
+                $('.container').append(cardItem);
+
                 cardHandler();
                 scrollHandler(usersUrl);
             });
@@ -18,42 +19,48 @@ const addCards = (usersUrl) => {
 };
 
 const cardHandler = () => {
+    let popUpIndicator = 0;
     $('.card-item').each((index, item) => {
-        let popUpIndicator = 0;
         $(item).on('click', () => {
+            const avatar = $(item).find('.avatar');
+            const closeBtn = $(item).find('.close-btn');
+
             if (popUpIndicator === 1) {
                 popUpIndicator = 0;
+
                 $(item).removeClass('fullscreen-card-item');
-                $(item).find('.avatar').removeClass('fullscreen-avatar');
-                $(item).find('.close-btn').removeClass('close-btn-active');
+                avatar.removeClass('fullscreen-avatar');
+                closeBtn.removeClass('close-btn-active');
+
                 $('body').css('overflow-y', 'scroll'); 
             }
             else {
                 popUpIndicator = 1;
-                $(item).find('.close-btn').addClass('close-btn-active');
-                $(item).addClass('fullscreen-card-item');
-                $(item).find('.avatar').addClass('fullscreen-avatar');
                 
+                closeBtn.addClass('close-btn-active');
+                $(item).addClass('fullscreen-card-item');
+                avatar.addClass('fullscreen-avatar');
+
                 $('body').css('overflow-y', 'hidden');
             }
-        });       
+        });
     });
 };
-let scrollState = 0;
+
 const scrollHandler = (usersUrl) => {
-    $(window).off('scroll').on('scroll', function() {
+    let scrollState = 0;
+    $(window).off('scroll').on('scroll', () => {
         const windowRelativeBottom = document.documentElement.getBoundingClientRect().bottom;
         const windowClientHeight = document.documentElement.clientHeight;
         const lastUserID = $('.card-item:last').attr('id');
-        console.log(windowRelativeBottom)
         
-        if (windowRelativeBottom > windowClientHeight + 500 && scrollState === 1) {
-            scrollState = 0;
-        } if (windowRelativeBottom < windowClientHeight + 500 && scrollState === 0) {
-            scrollState = 1;            
+        if (windowRelativeBottom < windowClientHeight + 500 && scrollState === 0) {
+            scrollState = 1;
             const newUrl = `${usersUrl.slice(0 , usersUrl.length - 2)}${lastUserID}`;
             addCards(newUrl);
-        }        
+        } else {
+            scrollState = 0;
+        }
     });
 };
 
